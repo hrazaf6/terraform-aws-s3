afd53f7f6ba8d82b26489770c8c31a81c691878e
Hamid Raza <hrazaf6@gmail.com>
2020-02-06 19:33:07 +0530

    added variable file

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "default" {
-  bucket = format("%s-%s",var.account_id,var.bucket_name)
+  bucket = format("%s--%s",var.account_id,var.bucket_name)
   acl    = "private"
 
   tags = {
-    Name        = format("%s-%s",var.account_id,var.bucket_name)
+    Name        = format("%s--%s",var.account_id,var.bucket_name)
     Environment = var.env
   }
 }
